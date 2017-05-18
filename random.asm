15fb8d0212613ad789e556e779935f975a0055c2
Ori Levi <ori1991@gmail.com>
2017-05-18 23:21:13 +0300

    add random procedure

---
+++ random.asm
@@ -0,0 +1,43 @@
+proc GenerateRandNum
+    ; --------------------------
+    ; GenerateRandNum Proc
+    ; Generates a pseudo-random 15-bit number.
+    ;
+    ; NOTE:
+    ;   your code MUST contains a variable with the name RNG_Seed with initial seed to the random algorithem.
+    ;
+    ; Return Value -> AX:
+    ;   AX contains the random number.
+    ; --------------------------
+   push bx cx si di
+
+   ; 32-bit multiplication in 16-bit mode (DX:AX * CX:BX == SI:DI)
+   mov  ax, [RNG_Seed]
+   xor  dx, dx
+   mov  cx, 041C6h
+   mov  bx, 04E6Dh
+   xor  di, di
+   push ax
+   mul  bx
+   mov  si, dx
+   xchg di, ax
+   mul  bx
+   add  si, ax
+   pop  ax
+   mul  cx
+   add  si, ax
+
+   ; Do addition
+   add  di, 3039h
+   adc  si, 0
+
+   ; Save seed
+   mov  [RNG_Seed], di
+
+   ; Get result and mask bits
+   mov  ax, si
+   and  ah, 07Fh
+
+   pop  di si cx bx
+   ret
+endp GenerateRandNum
